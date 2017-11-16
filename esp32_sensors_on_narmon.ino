4e82916923f09bbbece78dbbdd933628b54c27f8
Oxy976 <ghost_mail2005-musor@yahoo.com>
2017-11-16 15:51:52 +0300

    +OTA

--- esp32_sensors_on_narmon.ino
+++ esp32_sensors_on_narmon.ino
@@ -58,6 +58,10 @@ SoftwareSerial co2Serial(MH_Z19_RX, MH_Z19_TX); // define MH-Z19
 RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
 */
 
+// OTA
+#include <ArduinoOTA.h>
+
+
 
 WiFiClient wifiClient;
 PubSubClient client(server, 1883, wifiClient);
@@ -159,14 +163,64 @@ ds18b20_init(DS_PIN);
   Serial.println("------------------------------------");
 
 //MH-Z19
-
 Delay(300); // задержка на разогрев MH-Z19
+
+//======ArduinoOTA
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("OTA Ready");
+  Serial.print("OTA IP address: ");
+  Serial.println(WiFi.localIP());
+// ==============================
 }
 
 
 
 void loop()
 {
+//ArduinoOTA
+ArduinoOTA.handle();
+
 // считаем тики, как вышло время - собираем данные с других датчиков и отправляем...
 unsigned long currentMillis = millis();
  if(currentMillis - previousMillis > LOG_PERIOD){
