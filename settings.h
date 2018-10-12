627396c915d028fc567bb4d8f8e595122fe5be8e
Oxy976 <ghost_mail2005-musor@yahoo.com>
2018-10-12 14:22:39 +0300

    -dht sensor, +internal bme280 sensor.

--- settings.h
+++ settings.h
@@ -33,8 +33,9 @@ char clientId[] = MAC;
 char conntopic[] = TOPIC "status";
 
 
-//***BME280
-const int BME_ADDR=0x77; //I2C using  0x77 (default) or 0x76
+//***BME280 - I2C using  0x77 (default) or 0x76
+const int BME_ext_ADDR=0x77; 
+const int BME_int_ADDR=0x76; //I2C using  0x77 (default) or 0x76
 
 //***Geiger
 /* pin that is attached to interrupt 12 = ESP32 GIO12 */
@@ -45,15 +46,6 @@ const int LOG_PERIOD=6000000;  //Logging period //100000 - 10s, 600000 - 60s,1m 
 //J305?
 const float CF = 0.0052; //different for different tubes. The conversion factor (CF) for the official tube J305? is 0.008120 or CF=0.0058 ?
 
-
-
-//***DHT
-#define DHTPIN 25  //#GPIO25
-
-// Uncomment the type of sensor in use:
-//#define DHTTYPE           DHT11     // DHT 11 
-#define DHTTYPE           DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
 
 //distance 
 //sensor FC-51
