f0c83831868fce62822a4728666a73be07e25757
Oxy976 <ghost_mail2005-musor@yahoo.com>
2021-09-01 08:43:05 +0300

    Update OutToScr.cpp
    clean

--- src/OutToScr.cpp
+++ src/OutToScr.cpp
@@ -2,28 +2,21 @@
 #include <M5Stack.h>
 // https://github.com/m5stack/M5Stack/blob/master/src/utility/In_eSPI.h
 
-
-//#include "fonts/M5_Font_24px.h"
-//#include "fonts/M5_Font_32px.h"
-//#include "fonts/M5_Font_48px.h"
-//#include "fonts/verdana_24px.h"
-//#include "fonts/cl_24px.h"
 #include "fonts/RoboFix_24px.h"
 #include "fonts/RoboFix_28px.h"
 #include "fonts/RoboFix_72px.h"
 
-int sym_dnum[] = {0x00C0, 0x00C1, 0x00C2, 0x00C3, 0x00C4, 0x00C5, 0x00C6, 0x00C7, 0x00C8, 0x00C9 }; //!!only for font RobotoR !!
-int sym_gradC = 0x00D0; //!!only for font RobotoR !!
-int sym_uS = 0x00CC; //!!only for font RobotoR !!
-int sym_uR = 0x00CD; //!!only for font RobotoR !!
-int sym_mmR = 0x00CF; //!!only for font RobotoR !!
+int sym_dnum[] = {0x00C0, 0x00C1, 0x00C2, 0x00C3, 0x00C4, 0x00C5, 0x00C6, 0x00C7, 0x00C8, 0x00C9 }; //!!only for font Robo !!
+int sym_gradC = 0x00D0; //!!only for font Robo !!
+int sym_uS = 0x00CC; //!!only for font Robo !!
+int sym_uR = 0x00CD; //!!only for font Robo !!
+int sym_mmR = 0x00CF; //!!only for font Robo !!
 int sym_proc = 0x0025;
 
 
 void OutStrToScr( String stb1, String  stsr1, String  stsr2, String  stsd1, String  stsd2, String  stsd3 )
 {
   M5.Lcd.fillScreen(0);
-  M5.Lcd.setFreeFont(&RoboFix_72px);
 
 // решетка
   M5.Lcd.drawRoundRect(1, 1, 212, 160, 7, TFT_DARKGREY);
@@ -34,26 +27,21 @@ void OutStrToScr( String stb1, String  stsr1, String  stsr2, String  stsd1, Stri
   M5.Lcd.drawRoundRect(213, 160, 106, 80, 7, TFT_DARKGREY);
 
 // темп. улица
+  M5.Lcd.setFreeFont(&RoboFix_72px);
   M5.Lcd.setTextSize(1);
-  //M5.Lcd.setTextDatum(MC_DATUM); //Middle centre
-
   M5.Lcd.setTextColor(TFT_RED);
-
+  
   M5.Lcd.setTextDatum(MR_DATUM);
   M5.Lcd.drawString(stb1, 108, 80, 1);
-
   M5.Lcd.setTextDatum(ML_DATUM);
   M5.Lcd.drawString(String(char(sym_gradC)), 136, 80, 1);
 
 // даннце улицы, правый столбик
-  M5.Lcd.setTextSize(1);
-
-  M5.Lcd.setTextColor(TFT_MAGENTA);
-//  M5.Lcd.setFont(F_RR16);
   M5.Lcd.setFreeFont(&RoboFix_28px);
   M5.Lcd.setTextSize(1);
+  
   M5.Lcd.setTextColor(TFT_MAGENTA);
-
+  
   M5.Lcd.setTextDatum(MR_DATUM);
   M5.Lcd.drawString(stsr1, 265, 40, 1);
   M5.Lcd.setTextDatum(ML_DATUM);
@@ -64,38 +52,24 @@ void OutStrToScr( String stb1, String  stsr1, String  stsr2, String  stsd1, Stri
   M5.Lcd.setTextDatum(ML_DATUM);
   M5.Lcd.drawString(String(char(sym_mmR)), 283, 120, 1);
 
-//Внутри, нижеяя строка
-  //M5.Lcd.setTextColor(0xff80);
+//Внутри, нижняя строка
   M5.Lcd.setTextColor(TFT_YELLOW);
-//  M5.Lcd.setFont(F_RR16);
-  M5.Lcd.setFreeFont(&RoboFix_28px);
 
-//  M5.Lcd.drawString(stsd1, 53, 200, 1);
   M5.Lcd.setTextDatum(MR_DATUM);
   M5.Lcd.drawString(stsd1, 55, 200, 1);
   M5.Lcd.setTextDatum(ML_DATUM);
   M5.Lcd.drawString(String(char(sym_gradC)), 70, 200, 1);
 
-
-//  M5.Lcd.drawString(stsd2, 159, 200, 1);
   M5.Lcd.setTextDatum(MR_DATUM);
   M5.Lcd.drawString(stsd2, 159, 200, 1);
   M5.Lcd.setTextDatum(ML_DATUM);
   M5.Lcd.drawString(String(char(sym_proc)), 173, 200, 1);
 
-
-//  M5.Lcd.drawString(stsd3, 265, 200, 1);
   M5.Lcd.setTextDatum(MR_DATUM);
   M5.Lcd.drawString(stsd3, 258, 200, 1);
   M5.Lcd.setTextDatum(ML_DATUM);
   M5.Lcd.drawString(String(char(sym_uR)), 275, 200, 1);
-
-
-
 }
-
-
-
 
 
 String TempToStr(float t)
@@ -107,7 +81,6 @@ String TempToStr(float t)
   t1 = T / 10;
   t2 = abs(T % 10);
   sym_t2 = sym_dnum[t2];
-//  strT = String(t1) + "," + String(char(sym_t2)) + "  " + String(char(sym_gradC));
   strT = String(t1) + "," + String(char(sym_t2));
   return strT;
 }
@@ -116,9 +89,7 @@ String PressToStr(float p)
 {
   int p1;
   String strP;
-
   p1 = abs(p);
-//  strP = String(p1) + "  " + String(char(sym_mmR));
   strP = String(p1) ;
   return strP;
 }
@@ -132,7 +103,6 @@ String HumToStr(float h)
   h1 = H / 10;
   h2 = abs(H % 10);
   sym_h2 = sym_dnum[h2];
-//  strH = String(h1) + "," + String(char(sym_h2)) + "  %";
   strH = String(h1) + "," + String(char(sym_h2));
   return strH;
 }
@@ -146,7 +116,6 @@ String RgToStr(float r)
   r1 = R / 10;
   r2 = abs(R % 10);
   sym_r2 = sym_dnum[r2];
-//  strR = String(r1) + "," + String(char(sym_r2)) + "  " + String(char(sym_uR));
   strR = String(r1) + "," + String(char(sym_r2));
   return strR;
 }
